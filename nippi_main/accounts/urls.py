d9395fa45e850c342f5f0d569b66958d88265d26
Girish Amudala <amudalagirish@gmail.com>
2023-08-02 17:30:53 +0530

    User rigstration form created

--- nippi_main/accounts/urls.py
+++ nippi_main/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('registerUser/', views.resgisterUser, name='resgisterUser')
+    path('registerUser/', views.resgisterUser, name='registerUser')
 ]
